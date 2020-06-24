74f36e589d6442c19438b9aa99a738f580e7751f
Pedro Leda <pedro.henrique.leda@gmail.com>
2020-06-23 21:24:52 -0300

    1 aula - animações da personagem e cenario

---
+++ Little_Witch_2020_06_24_00_21_02/sketch.js
@@ -0,0 +1,25 @@
+let imagemCenario;
+let imagemPersonagem;
+let cenario;
+let personagem;
+let somDoJogo;
+
+function preload(){
+  imagemCenario = loadImage('imagens/cenario/floresta.png');
+  imagemPersonagem = loadImage('imagens/personagem/correndo.png');
+  somDoJogo = loadSound('sons/trilha_jogo.mp3');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  cenario = new Cenario(imagemCenario, 5);
+  personagem = new Personagem(imagemPersonagem);
+  frameRate(40);
+  somDoJogo.loop();
+}
+ 
+function draw() {
+  cenario.exibe();
+  cenario.move();
+  personagem.exibe();  
+}
